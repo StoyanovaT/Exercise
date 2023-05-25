78f754169eab7a0bd6beb7c485ea250edf0840c5
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2023-05-25 15:38:03 +0300

    Additional work

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
 from collections import deque
 
-water = int(input())
-name = input()
-people_line = deque()
+kids = deque(input().split())
+toss = int(input())
 
-while not name == "Start":
-    people_line.append(name)
+while len(kids) > 1:
+    kids.rotate(-toss)
+    print(f"Removed {kids.pop()}")
 
-    name = input()
+print(f"Last is {kids.pop()}")
 
-command = input()
-while not command == "End":
-    if command.isdigit():
-        amount = int(command)
-        current_name = people_line.popleft()
-        if water >= amount:
-            water -= amount
-            print(f"{current_name} got water")
-        else:
-            print(f"{current_name} must wait")
-
-    else:
-        _, amount = command.split()
-        water += int(amount)
-
-    command = input()
-
-print(f"{water} liters left")
 
 
