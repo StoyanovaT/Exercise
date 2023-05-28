81c23620c8c55541709a974cd9aefd0e51184ab8
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2023-05-28 15:53:41 +0300

    Final code

--- fast_food_03.py
+++ fast_food_03.py
@@ -1,8 +1,19 @@
 from collections import deque
 
 food = int(input())
-
 orders = deque([int(x) for x in input().split()])
-
 print(max(orders))
 
+for index in range(len(orders)):
+    if food >= orders[0]:
+        current_order = orders.popleft()
+        food -= current_order
+
+    else:
+        break
+
+if orders:
+    print(f"Orders left: {' '.join([str(x) for x in orders])}")
+
+else:
+    print("Orders complete")
