d5cccb048605d01d35908e23c88d72b0d567f877
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2023-05-29 18:01:13 +0300

    New solution

---
+++ balanced_parentheses_06.py
@@ -0,0 +1,24 @@
+expression = [str(x) for x in input()]
+
+opening = []
+balanced = True
+
+for i in expression:
+    if i in ['{', '[', '(']:
+        opening.append(i)
+    else:
+        if opening:
+            op = opening.pop()
+            if op == '{' and i == '}' or op == '[' and i == ']' or op == '(' and i == ')':
+                continue
+            else:
+                balanced = False
+                break
+        else:
+            balanced = False
+            break
+
+if balanced:
+    print('YES')
+else:
+    print('NO')
