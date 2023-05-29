99520d225040a5651d47b003d33c7d0d4180aff6
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2023-05-29 22:09:43 +0300

    Some functions

--- robotics_07.py
+++ robotics_07.py
@@ -20,13 +20,30 @@ process_time_by_robot = {}
 busy_until_by_robot = {}
 products_deque = deque()
 
-start_time_in_sec = convert_str_in_seconds(input())
+time = convert_str_in_seconds(input())
 
 for robot_data in robots_data:
     robot, time_busy = robot_data.split('-')
     process_time_by_robot[robot] = int(time_busy)
-    busy_until_by_robot[robot] = 0
+    busy_until_by_robot[robot] = -1
 
 while True:
-    product =
+    product = input()
+    if product != 'End':
+        products_deque.append(product)
+    else:
+        break
+
+while products_deque:
+    product = products_deque.popleft()
+    time += 1
+    for name, busy_until in busy_until_by_robot.items():
+        if time >= busy_until:
+            busy_until_by_robot[name] = time + process_time_by_robot[name]
+            print(f"{name} - {product} [{convert_seconds_in_str(time)}]")
+        else:
+            products_deque.append(product)
+            continue
+
+
 
