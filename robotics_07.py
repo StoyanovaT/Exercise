1582ac5b1de7e6bec134478861c21a2f67d61d76
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2023-05-29 22:22:28 +0300

    Final

--- robotics_07.py
+++ robotics_07.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 
-def convert_str_in_seconds(str_time):
+def convert_str_to_seconds(str_time):
     hours, minutes, seconds = [int(x) for x in str_time.split(':')]
     return hours * 60 * 60 + minutes * 60 + seconds
 
@@ -18,32 +18,34 @@ def convert_seconds_in_str(seconds):
 robots_data = input().split(';')
 process_time_by_robot = {}
 busy_until_by_robot = {}
-products_deque = deque()
-
-time = convert_str_in_seconds(input())
 
 for robot_data in robots_data:
     robot, time_busy = robot_data.split('-')
     process_time_by_robot[robot] = int(time_busy)
     busy_until_by_robot[robot] = -1
 
+time = convert_str_to_seconds(input())
+products_deque = deque()
+
 while True:
     product = input()
-    if product != 'End':
-        products_deque.append(product)
-    else:
+    if product == 'End':
         break
+    products_deque.append(product)
 
 while products_deque:
-    product = products_deque.popleft()
     time += 1
+    product = products_deque.popleft()
+
     for name, busy_until in busy_until_by_robot.items():
         if time >= busy_until:
             busy_until_by_robot[name] = time + process_time_by_robot[name]
             print(f"{name} - {product} [{convert_seconds_in_str(time)}]")
-        else:
-            products_deque.append(product)
-            continue
+            break
+    else:
+        products_deque.append(product)
+
+
 
 
 
